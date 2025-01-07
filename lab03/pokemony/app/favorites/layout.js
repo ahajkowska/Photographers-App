e754dbbf9f443c47555da6b131f88adfdaa31d05
Amelia Hajkowska <ameliahajkowska@gmail.com>
2025-01-07 18:26:27 +0100

    lab03 fix

--- lab03/pokemony/app/favorites/layout.js
+++
@@ -1,8 +0,0 @@
-export default function FavoritesLayout({ children }) {
-    return (
-        <div>
-            <h1>Favorites</h1>
-            {children}
-        </div>
-    );
-}
--- lab03/pokemony/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Hello!</h1>
-            <p>Use the navigation above to explore the app.</p>
-        </div>
-    );
-}
