05a370cd25e1ad05d578c0df4ebb5d2ed0d7e1a2
Amelia Hajkowska <156909419+ahajkowska@users.noreply.github.com>
2025-02-20 17:16:41 +0100

    Delete lab04 directory

--- lab04/app/app/pokemon/[id]/layout.js
+++
@@ -1,8 +0,0 @@
-export default function PokemonDetailsLayout({ children }) {
-    return (
-        <div className="pokemon-layout">
-            <h1>Szczegóły Pokemona</h1>
-            {children}
-        </div>
-    );
-}
