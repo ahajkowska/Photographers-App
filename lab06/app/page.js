bc7598043317f853145935d1059f93d6e0caadd9
Amelia Hajkowska <156909419+ahajkowska@users.noreply.github.com>
2025-02-20 17:16:56 +0100

    Delete lab06 directory

--- lab06/app/page.js
+++
@@ -1,9 +0,0 @@
-import StatsPanel from './components/StatsPanel';
-
-export default async function HomePage() {
-    return (
-      <div className="homepage">
-        <StatsPanel />
-      </div>
-    );
-};
